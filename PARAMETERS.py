311c39a18f16efda2eb8de007057c58c89891a3c
Pascal Martinolli <pascal.umontreal@gmail.com>
2023-05-11 16:07:30 -0400

    Add files via upload

--- PARAMETERS.py
+++ PARAMETERS.py
@@ -6,8 +6,9 @@ Created on Tue Apr 11 16:31:06 2023
 """
 
 # Set the path to the directory containing the PDF files
-folder_path = r'E:/00-Yragatheque/_compilRPGrules' 
+folder_path = r'D:/00-Yragatheque/_compilRPGrules/' 
 
+working_directory = "_working_directory/"
 
-# folder_path = r'C:\00-Yragatheque\test' 
-# folder_path = r'C:\00-Yragatheque\0-9' 
+#folder_path = r'C:/00-Yragatheque/test/' 
+# folder_path = r'C:/00-Yragatheque/0-9/' 
